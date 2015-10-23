f0b0635482ad12be6ebdeed17b1b9c57df3dbb20
Anthony Chu <anthony.chu@liferay.com>
2015-10-23 12:16:02 -0700

    Build scripts

---
+++ build-test.sh
@@ -0,0 +1,130 @@
+source "c:/users/liferay/.bashrc"
+
+baseDir=c:/users/liferay/Desktop
+portalDir=d:/public/master-portal
+implDir=$portalDir/portal-impl
+newDir=d:/test-results
+
+clean(){
+	echo "[INFO] Cleaning out old test results..."
+	rm -rf $newDir
+	cd d:/
+	mkdir $newDir
+	echo "[INFO] DONE."
+	cd $baseDir
+}
+
+newbranch(){
+	echo "[INFO] Checking out a new branch: $1"
+
+	cd $portalDir
+
+	git checkout -q -b $1
+
+	cd $baseDir
+}
+
+pr(){
+	echo "[INFO] Submitting pull request..."	
+	if (( !"$#" )); then
+		echo "[ERROR] Missing branch, reviewer, comment, and title."
+	else
+		echo "  branch:   $1"
+		echo "  reviewer: $2"
+		echo "  comment:  $3"
+		echo "  title:    $4"
+		echo
+		cd $portalDir
+		gitpr -b $1 -u $2 submit $3 $4
+		cd $baseDir
+	fi
+}
+
+rebase(){
+	echo "[INFO] Updating to HEAD and rebasing commits..."
+	echo
+	cd $portalDir
+	git pull --rebase upstream master
+	echo "[INFO] DONE."
+	echo
+	cd $baseDir
+}
+
+sf(){
+	echo "[INFO] Running source formatter..."
+	echo
+	cd $implDir
+	ant format-source
+	echo "[INFO] DONE."
+	echo
+	cd $baseDir
+}
+
+showbranch(){
+	echo "[INFO] Listing all branches..."
+	cd $portalDir
+
+	git branch -q --list
+
+	cd $baseDir
+}
+
+switch(){
+	echo "[INFO] Moving to branch $1..."
+	cd $portalDir
+
+	git checkout -q $1
+
+	cd $baseDir
+}
+
+test(){
+	if (( !"$#" )); then
+		echo "[ERROR] Missing test name."
+	else
+		echo "[INFO] Running test $1..."
+		echo
+		cd $portalDir
+		ant -f build-test.xml run-selenium-test -Dtest.class="$1"
+
+		testname=$1
+		testname=${testname//[#]/_}
+
+		resultDir=${portalDir}/portal-web/test-results/${testname}
+
+		echo "[INFO] Moving test results..."
+		echo
+		mv ${resultDir}/index.html ${newDir}/$1_index.html
+
+		cd $baseDir
+	fi
+}
+
+validate(){
+	echo "[INFO] Running POSHI validation..."
+	echo
+	cd $portalDir
+
+	ant -f build-test.xml run-poshi-validation
+
+	echo "[INFO] DONE."
+
+	cd $baseDir
+}
+
+main(){
+	if (( !"$#" )); then
+		echo "Usage: $0 (commands...)"
+		printf "Commands:\n    sf      runs source formatter\n    test      Runs a POSHI test\n    validate  Verifies that POSHI files are properly formatted\n    rebase  pulls current branch up to HEAD and rebases local commits on top\n    pr      makes a pull request"
+		exit
+	else
+		if [[ $# > 1 ]]; then
+			$1 ${@:2}
+		else
+			$1
+		fi
+	fi
+}
+
+clear
+main $@
---
+++ bundle.sh
@@ -0,0 +1,177 @@
+baseDir=c:/users/liferay/Desktop
+
+_build_log(){
+	timestamp=$(_timestamp_clock)
+	timestamp=${timestamp//[:]/}
+
+	logStructure=(d logs ${branch} $(_timestamp_date))
+
+	for (( i=0; i<${#logStructure}; i++ )); do
+		logDir=${logDir}/${logStructure[i]}
+		if [ ! -e $logDir ]; then
+			mkdir $logDir
+		else
+			cd $logDir
+		fi
+	done
+
+	export logFile=$logDir/$branch-build-$(_gitlog)-${timestamp}.log
+}
+
+build(){
+	_build_log
+
+	_clean_hard
+
+	cd $buildDir
+
+	echo "[INFO] Unzipping tomcat..."
+	ant -f build-dist.xml unzip-tomcat
+	echo "[INFO] DONE."
+
+	_config
+
+	echo "[INFO] Building portal..."
+	ant all > $logFile | tail -f --pid=$$ "$logFile"
+	echo "[INFO] Build complete. Please see the build log for details."
+	cd $baseDir
+}
+
+_clean_database(){
+	echo "[INFO] Rebuilding database..."
+	mysql -e "drop database if exists $database; create database $database char set utf8;"
+	echo "[INFO] DONE."
+	echo
+	cd $baseDir
+}
+
+_clean_hard(){
+	echo "[INFO] Deleting all files and folder in the bundles directory..."
+	cd $bundleDir
+	rm -rf $bundleDir/data $bundleDir/deploy $bundleDir/logs $bundleDir/osgi
+	rm -rf $tomcatDir
+	echo "[INFO] DONE."
+	cd $baseDir
+}
+
+_clean_liferay_home(){
+	echo "[INFO] Deleting liferay home folders..."
+	cd ${bundleDir}
+	rm -rf $bundleDir/data $bundleDir/logs
+	echo "[INFO] DONE."
+	echo
+	cd $baseDir
+}
+
+_clean_temp_files(){
+	echo "[INFO] Deleting temp files..."
+	cd $tomcatDir
+	rm -rf temp work
+	echo "[INFO] DONE."
+	echo
+	cd $baseDir
+}
+
+_config(){
+	echo "[INFO] Increasing memory limit..."
+	sed -i "s/-Xmx1024m/-Xmx2048m/g" $tomcatDir/bin/setenv.sh
+	sed -i "s/-XX:MaxPermSize=256m/-XX:MaxPermSize=512m/g" $tomcatDir/bin/setenv.sh
+	echo "[INFO] DONE."
+
+	if [[ $branch == ee-6.2.x ]]; then
+		echo "[INFO] Changing port for ee-6.2.x..."
+		sed -i "s/\"8/\"7/g" $tomcatDir/conf/server.xml
+	fi
+
+clean(){
+	_clean_database
+	_clean_liferay_home
+	_clean_temp_files
+}
+
+_gitlog(){
+	cd $buildDir
+	git log --oneline --pretty=format:%h -1
+	cd $baseDir
+}
+
+getDirs(){
+	args=$@
+
+	if [[ $args == *master* ]]; then
+		branch=master
+	elif [[ $args == *ee-6.2.x* ]]; then
+		branch=ee-6.2.x
+	else
+		branch=master
+	fi
+
+	args=${args/$branch/""}
+
+	case $branch in
+		"master") buildDir=d:/public/master-portal; bundleDir=d:/public/master-bundles; database=lportalmaster;;
+		"ee-6.2.x") buildDir=d:/private/ee-6.2.x-portal; bundleDir=d:/private/ee-6.2.x-bundles; database=lportal;;
+	esac
+
+	export args="${args}" branch="${branch}" buildDir="${buildDir}" bundleDir="${bundleDir}" database="${database}" tomcatDir="$bundleDir/tomcat-7.0.62/"
+}
+
+getDirs $@
+
+pull(){
+	cd $buildDir
+	git reset --hard -q
+	git checkout $branch -q
+	git clean -fdqx -e "*.anthonychu.properties"
+
+	echo "[INFO] Pulling changes from upstream..."
+	git pull upstream $branch
+	echo "[INFO] DONE."
+	cd $baseDir
+}
+
+push(){
+	cd $buildDir
+
+	echo "[INFO] Pushing changes to origin..."
+	git push -f origin $branch
+	echo "[INFO] DONE."
+	cd $baseDir
+}
+
+run(){
+	echo "[INFO] Starting server..."
+	sleep 5s
+	clear
+	$tomcatDir/bin/catalina.sh run # | firefox
+}
+
+_timestamp_clock(){
+	date +%T%s
+}
+
+_timestamp_date(){
+	date +%Y%m%d
+}
+
+main(){
+	if (( !"$#" )); then
+		echo "Usage: $0 (commands...)"
+		echo "Commands:"
+		echo "    build     builds a bundle"
+		echo "    clean     prepares a fresh bundle"
+		echo "    pull      pulls changes from upstream"
+		echo "    push      pushes changes to origin"
+		echo "    run       runs a bundle"
+		exit
+	fi
+
+	while (( "$#" )); do
+		$1
+		shift
+	done
+}
+
+clear
+getDirs $@
+main $args
