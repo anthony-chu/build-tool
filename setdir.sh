096d04d9e58aaf3e9a08613542ee0c7ceb8ed4d1
Anthony Chu <anthony.chu@liferay.com>
2015-11-19 07:48:24 -0800

    Extract directory definition into its own file

---
+++ setdir.sh
@@ -0,0 +1,28 @@
+getBaseDir(){
+  export baseDir=$(PWD)
+}
+
+getDirs(){
+  args=$@
+
+  if [[ $args == *master* ]]; then
+    branch=master
+  elif [[ $args == *ee-6.2.x* ]]; then
+    branch=ee-6.2.x
+  else
+    branch=master
+  fi
+
+  if [[ $args == *pr* ]]; then
+    args=${args}
+  else
+    args=${args/$branch/""}
+  fi
+
+  case $branch in
+    "ee-6.2.x") buildDir=d:/private/ee-6.2.x-portal; bundleDir=d:/private/ee-6.2.x-bundles; database=lportal;;
+    "master") buildDir=d:/public/master-portal; bundleDir=d:/public/master-bundles; database=lportalmaster;;
+  esac
+
+  export args="${args}" branch="${branch}" buildDir="${buildDir}" bundleDir="${bundleDir}" database="${database}" tomcatDir="$bundleDir/tomcat-7.0.62"
+}
