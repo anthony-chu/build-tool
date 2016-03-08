793f5d1debd41ee9c4d189032c73abb26915c168
Anthony Chu <anthony.chu@liferay.com>
2016-03-08 14:03:29 -0800

    Deprecate tomcatDir and tomcatVersion variables

--- setdir.sh
+++ setdir.sh
@@ -11,21 +11,16 @@ getDirs(){
 
     if [[ $@ == *master* ]]; then
         branch=master
-        tomcatVersion=8.0.32
     elif [[ $@ == *ee-6.2.x* ]]; then
         branch=ee-6.2.x
-        tomcatVersion=7.0.62
     elif [[ $@ == *ee-7.0.x* ]]; then
         branch=ee-7.0.x
-        tomcatVersion=8.0.32
     else
         branch=master
-        tomcatVersion=8.0.32
     fi
 
     buildDir=d:/${privacy}/${branch}-portal
     bundleDir=d:/${privacy}/${branch}-bundles
     database=lportal${branch//[-.]/""}
 
-    export branch="${branch}" buildDir="${buildDir}" bundleDir="${bundleDir}" database="${database}" tomcatDir="$bundleDir/tomcat-$tomcatVersion"
-}
+    export branch="${branch}" buildDir="${buildDir}" bundleDir="${bundleDir}" database="${database}"
