163e355f03982de030792d9ae4cd1d224c656164
Anthony Chu <anthony.chu@liferay.com>
2016-03-09 10:24:30 -0800

    More space

--- test.sh
+++ test.sh
@@ -28,7 +28,7 @@ pr(){
 		detailText=("$branch" "$1" "$comment" "$title")
 
 		for (( i=0; i<${#detailText[@]}; i++)); do
-			echo "    ${newDetailHeading[i]}..........${detailText[i]}"
+			echo "    ${newDetailHeading[i]}................${detailText[i]}"
 		done
 
 		echo
