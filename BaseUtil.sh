8b3f3ab83db88e2268f585e2a32138eb3550f279
Anthony Chu <anthony.chu@liferay.com>
2016-03-09 11:38:12 -0800

    Move into BaseUtil

--- BaseUtil.sh
+++ BaseUtil.sh
@@ -1,11 +1,15 @@
-gitpr(){
-	alias gitpr="source d:/git-tools/git-pull-request/git-pull-request.sh"
-	source "c:/users/liferay/.bashrc"
-	source "d:/git-tools/git-pull-request/git-pull-request.sh"
-}
+BaseUtil(){
+	gitpr(){
+		alias gitpr="source d:/git-tools/git-pull-request/git-pull-request.sh"
+		source "c:/users/liferay/.bashrc"
+		source "d:/git-tools/git-pull-request/git-pull-request.sh"
+	}
 
-getOption(){
-	if [[ $1 == -[a-zA-Z0-9]* ]]; then
-		echo ${1/-/ }
-	fi
+	getOption(){
+		if [[ $1 == -[a-zA-Z0-9]* ]]; then
+			echo ${1/-/ }
+		fi
+	}
+
+	$@
 }
