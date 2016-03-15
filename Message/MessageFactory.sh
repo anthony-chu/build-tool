b2745cce97662bf5578491d655e502ff06346425
Anthony Chu <anthony.chu@liferay.com>
2016-03-15 14:50:02 -0700

    Add methods to build messages

---
+++ Message/MessageFactory.sh
@@ -0,0 +1,31 @@
+source Base/BaseUtil.sh
+
+MessageFactory(){
+    local time=$(BaseUtil timestamp)
+
+    _buildMessage(){
+        local message=$@
+
+        echo "$time [${logLevel^^}] $message."
+    }
+
+    printDone(){
+        printInfoMessage DONE
+    }
+
+    printErrorMessage(){
+        local logLevel=error
+        local message=$@
+
+        _buildMessage $message
+    }
+
+    printInfoMessage(){
+        local logLevel=info
+        local message=$@
+
+        _buildMessage $message
+    }
+
+    $@
+}
