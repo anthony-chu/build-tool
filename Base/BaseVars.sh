b31d77db33d6722a6cf92743ca650dc389b45602
Anthony Chu <anthony.chu@liferay.com>
2016-03-21 12:31:16 -0700

    Add 6.2.x branch name

--- Base/BaseVars.sh
+++ Base/BaseVars.sh
@@ -15,6 +15,7 @@ BaseVars(){
         case $@ in
             *ee-6.2.x*) echo ee-6.2.x;;
             *ee-7.0.x*) echo ee-7.0.x;;
+			*6.2.x*) echo 6.2.x;;
             *master*) echo master;;
             *7.0.x*) echo 7.0.x;;
             *) echo master;;
