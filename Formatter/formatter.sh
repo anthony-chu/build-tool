ae0e92dbb2cc837f5cc6155da1cabddcd5a5ef91
Anthony Chu <anthony.chu@liferay.com>
2016-05-24 10:55:35 -0700

    Add formatter

---
+++ Formatter/formatter.sh
@@ -0,0 +1,22 @@
+Formatter(){
+	curFile=${0/.\//}
+	allFiles=($(find ../* -type f))
+	includedFiles=(${allFiles[@]/*$curFile/})
+
+	formatTabs(){
+		for (( i=0; i<${#includedFiles[@]}; i++ )); do
+			sed -i "s/	/\t/g" ${includedFiles[i]}
+		done
+	}
+
+	formatSpaceAfterTab(){
+		for (( i=0; i<${#includedFiles[@]}; i++ )); do
+			sed -i "s/\t /\t/g" ${includedFiles[i]}
+		done
+	}
+
+	$@
+}
+
+Formatter formatTabs
+Formatter formatSpaceAfterTab
