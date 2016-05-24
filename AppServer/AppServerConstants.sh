1f1125bc26d32428fbe1127d455e1ef1da0a4574
Anthony Chu <anthony.chu@liferay.com>
2016-05-24 09:52:02 -0700

    Make app server versions constants

---
+++ AppServer/AppServerConstants.sh
@@ -0,0 +1,47 @@
+AppServerConstants(){
+	glassfishVersion(){
+        echo "3.1.2.2"
+    }
+
+    jettyVersion(){
+        echo "8.1.10"
+    }
+
+    jbossVersion(){
+        echo "eap-6.4.0"
+    }
+
+    jonasVersion(){
+        echo "5.2.3"
+    }
+
+    resinVersion(){
+        echo "4.0.44"
+    }
+
+    tcatVersion(){
+        echo "7.0.2"
+    }
+
+    tcserverVersion(){
+        echo "2.9.11"
+    }
+
+    tomcatVersion(){
+        echo "8.0.32"
+    }
+
+    weblogicVersion(){
+        echo "12.1.3"
+    }
+
+    websphereVersion(){
+        echo "8.5.5.0"
+    }
+
+    wildflyVersion(){
+        echo "10.0.0"
+    }
+
+	$@
+}
