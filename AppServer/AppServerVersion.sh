f9983b5971d19be95096f9bdea6e1dffc831e27c
Anthony Chu <anthony.chu@liferay.com>
2016-05-24 11:28:04 -0700

    Use constants

--- AppServer/AppServerVersion.sh
+++ AppServer/AppServerVersion.sh
@@ -1,54 +1,11 @@
 source AppServer/AppServerConstants.sh
 
 AppServerVersion(){
-	_glassfishVersion(){
-		AppServerConstants glassfishVersion
-	}
 
-	_jettyVersion(){
-		AppServerConstants jettyVersion
-	}
-
-	_jbossVersion(){
-		AppServerConstants jbossVersion
-	}
-
-	_jonasVersion(){
-		AppServerConstants jonasVersion
-	}
-
-	_resinVersion(){
-		AppServerConstants resinVersion
-	}
-
-	_tcatVersion(){
-		AppServerConstants tcatVersion
-	}
-
-	_tcserverVersion(){
-		AppServerConstants tcserverVersion
-	}
-
-	_tomcatVersion(){
-		AppServerConstants tomcatVersion
-	}
-
-	_weblogicVersion(){
-		AppServerConstants weblogicVersion
-	}
-
-	_websphereVersion(){
-		AppServerConstants websphereVersion
-	}
-
-	_wildflyVersion(){
-		AppServerConstants wildflyVersion
-	}
-
-	returnAppServerVersion(){
+    returnAppServerVersion(){
 		local appServer=$1
 
-		echo $(_${appServer}Version)
+		AppServerConstants ${appServer}Version
 	}
 
 	$@
