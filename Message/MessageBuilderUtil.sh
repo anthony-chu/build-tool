f50642db568aca9c70454d402d24caf6ca81337b
Anthony Chu <anthony.chu@liferay.com>
2016-05-31 13:52:32 -0700

    Add intermediate step of capitalization before converting string

--- Message/MessageBuilderUtil.sh
+++ Message/MessageBuilderUtil.sh
@@ -5,7 +5,9 @@ MessageBuilderUtil(){
 	local time=$(BaseUtil timestamp)
 
 	buildMessage(){
-		echo "${time} [$(StringUtil toUpperCase ${1})] $(StringUtil replace $2 - space)"
+		_message=$(StringUtil capitalize $2)
+
+		echo "${time} [$(StringUtil toUpperCase ${1})] $(StringUtil replace $_message - space)"
 	}
 
 	$@
