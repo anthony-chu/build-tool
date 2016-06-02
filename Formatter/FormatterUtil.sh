3340b7d8d8460b8837b45059f5b75e8e91c44a59
Anthony Chu <anthony.chu@liferay.com>
2016-06-02 14:04:16 -0700

    Format variables

--- Formatter/FormatterUtil.sh
+++ Formatter/FormatterUtil.sh
@@ -1,13 +1,13 @@
 FormatterUtil(){
 	getExcludeStatus(){
-		if [[ $1 =~ $2 ]]; then
+		if [[ ${1} =~ ${2} ]]; then
 			isExcluded=true
 			break
 		else
 			isExcluded=false
 		fi
 
-		echo $isExcluded
+		echo ${isExcluded}
 	}
 
 	$@
