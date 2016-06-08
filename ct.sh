ecad2cc9b657f2e8b07c01ae96e03fd4d0d1ed33
Anthony Chu <anthony.chu@liferay.com>
2016-06-08 14:17:04 -0700

    Use _branchSwitcher and move messages up

--- ct.sh
+++ ct.sh
@@ -16,10 +16,28 @@ CTBuilder(){
 		fi
 	}
 
+	_branchSwitcher(){
+		curBranch=$(git rev-parse --abbrev-ref HEAD)
+
+		if [[ ${curBranch} == ${1} ]]; then
+			echo
+		else
+			if [[ $(git branch) == *${1}* ]]; then
+				git checkout ${1}
+			else
+				git checkout -b ${1}
+			fi
+		fi
+	}
+
 	build(){
 		MB printProgressMessage building-content-targeting-modules
 
 		cd ${projectDir}
+
+		branch=$(_branchChecker ${1})
+
+		_branchSwitcher ${branch}
 
 		d:/private/ee-7.0.x-portal/gradlew clean deploy
 
@@ -33,39 +51,19 @@ CTBuilder(){
 
 		branch=$(_branchChecker $1)
 
-		curBranch=$(git rev-parse --abbrev-ref HEAD)
-
-		if [[ ${curBranch} == ${branch} ]]; then
-			echo
-		else
-			if [[ $(git branch) == *${branch}* ]]; then
-				git checkout ${branch}
-			else
-				git checkout -b ${branch}
-			fi
-		fi
+		_branchSwitcher ${branch}
 
 		echo "Content Targeting GIT ID: $(git --git-dir=${projectDir}/.git rev-parse origin/${branch})"
 	}
 
 	release(){
+		MB printProgressMessage generating-a-release-zip-for-${branch}
+
 		cd ${projectDir}
 
 		branch=$(_branchChecker $1)
 
-		curBranch=$(git rev-parse --abbrev-ref HEAD)
-
-		if [[ ${curBranch} == ${branch} ]]; then
-			echo
-		else
-			if [[ $(git branch) == *${branch}* ]]; then
-				git checkout ${branch}
-			else
-				git checkout -b ${branch}
-			fi
-		fi
-
-		MB printProgressMessage generating-a-release-zip-for-${branch}
+		_branchSwitcher ${branch}
 
 		d:/private/ee-7.0.x-portal/gradlew release
 
@@ -75,23 +73,13 @@ CTBuilder(){
 	}
 
 	update(){
-		branch=$(_branchChecker $1)
-
 		MB printProgressMessage updating-content-targeting-on-branch-${branch}
 
 		cd ${projectDir}
 
-		curBranch=$(git rev-parse --abbrev-ref HEAD)
+		branch=$(_branchChecker ${1})
 
-		if [[ ${curBranch} == ${branch} ]]; then
-			echo
-		else
-			if [[ $(git branch) == *${branch}* ]]; then
-				git checkout ${branch}
-			else
-				git checkout -b ${branch}
-			fi
-		fi
+		_branchSwitcher ${branch}
 
 		git pull upstream ${branch}
 		git push origin ${branch}
