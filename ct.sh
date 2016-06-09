0aa637cb4a40ce9e72e9186e1e5ccb489296f594
Anthony Chu <anthony.chu@liferay.com>
2016-06-09 14:25:44 -0700

    Specify what is being checked

--- ct.sh
+++ ct.sh
@@ -33,7 +33,7 @@ CTBuilder(){
 
 			for (( i=0; i<${#allBranches[@]}; i++ )); do
 				if [[ $(SV isEqual ${allBranches[i]/remotes\/origin\//} ${1}) == false ]]; then
-					MB printErrorMessage the-branch-${1}-does-not-exist
+					MB printErrorMessage the-branch-${1}-does-not-exist-in-origin
 					doSwitch=false
 				else
 					doSwitch=true
