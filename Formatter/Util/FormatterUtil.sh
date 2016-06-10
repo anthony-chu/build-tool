6408a28ed8c8161b06f7b2090876f0e1b0fe57ff
Anthony Chu <anthony.chu@liferay.com>
2016-06-10 16:33:44 -0700

    Simplify

--- Formatter/Util/FormatterUtil.sh
+++ Formatter/Util/FormatterUtil.sh
@@ -3,13 +3,8 @@ source String/Validator/StringValidator.sh
 FormatterUtil(){
 	getExcludeStatus(){
 		isSubstring=$(StringValidator isSubstring ${1} ${2})
-		if [[ ${isSubstring} == true ]]; then
-			isExcluded=true
-		else
-			isExcluded=false
-		fi
 
-		echo ${isExcluded}
+		echo ${isSubstring}
 	}
 
 	$@
