052cfb4448978d4c328617da55d6aa258b791c8c
Anthony Chu <anthony.chu@liferay.com>
2016-06-15 12:30:36 -0700

    Include .liferay-home in zip

--- build.sh
+++ build.sh
@@ -347,7 +347,7 @@ zip(){
 
 	MB printProgressMessage zipping-up-${appServer}-bundle
 
-	content=(data deploy ${appServer}-${appServerVersion} osgi tools work)
+	content=(data deploy ${appServer}-${appServerVersion} osgi tools work .liferay-home)
 
 	jar -cMf ${zipFile} ${content[@]}
 	MB printDone
