8d19708bfce3a203ab5351d1cf90e5a47f4df3b8
Anthony Chu <anthony.chu@liferay.com>
2016-06-22 13:24:57 -0700

    Add 6.1 variables

--- Base/Vars/BaseVars.sh
+++ Base/Vars/BaseVars.sh
@@ -13,8 +13,10 @@ BaseVars(){
 
 	returnBranch(){
 		case $@ in
+			*ee-6.1.x*) echo ee-6.1.x;;
 			*ee-6.2.x*) echo ee-6.2.x;;
 			*ee-7.0.x*) echo ee-7.0.x;;
+			*6.1.x*) echo 6.1.x;;
 			*6.2.x*) echo 6.2.x;;
 			*master*) echo master;;
 			*7.0.x*) echo 7.0.x;;
