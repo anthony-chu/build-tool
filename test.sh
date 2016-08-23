48c011a4368a69ba53b2a5ab044939ee47744b10
Anthony Chu <anthony.chu@liferay.com>
2016-08-23 12:14:06 -0700

    Use function to make one check instead of 2.

--- test.sh
+++ test.sh
@@ -117,12 +117,12 @@ sf(){
 
 	cd ${implDir}
 
-	if [[ ${1} =~ a ]] || [[ ${1} =~ A ]]; then
+	if [[ $(Comparator isEqualIgnoreCase ${1} a) ]]; then
 		localChanges=""
 
 		${MB} printProgressMessage running-source-formatter-on-all-files
 		echo
-	elif [[ ${1} =~ l ]] || [[ ${1} =~ L ]]; then
+	elif [[ $(Comparator isEqualIgnoreCase ${1} l) ]]; then
 		localChanges="-local-changes"
 
 		${MB} printProgressMessage running-source-formatter-on${localChanges}
