1db7a1bb5205934c81f73e5f6e5ec9a416bfa1e1
Anthony Chu <anthony.chu@liferay.com>
2016-08-29 17:22:13 -0700

    Add functions for OS validation

---
+++ OS/Validator/OSValidator.sh
@@ -0,0 +1,21 @@
+include OS/Util/OSUtil.sh
+
+OSValidator(){
+	isLinux(){
+		if [[ $(OSUtil getOS) =~ Linux ]]; then
+			echo true
+		else
+			return
+		fi
+	}
+
+	isWindows(){
+		if [[ $(OSUtil getOS) =~ NT ]]; then
+			echo true
+		else
+			return
+		fi
+	}
+
+	$@
+}
