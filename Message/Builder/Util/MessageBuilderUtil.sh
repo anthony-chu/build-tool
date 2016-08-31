1c136cbe0b37ae02165f7824f8d6b2cf71689c2f
Anthony Chu <anthony.chu@liferay.com>
2016-08-31 13:47:44 -0700

    Simplify

--- Message/Builder/Util/MessageBuilderUtil.sh
+++ Message/Builder/Util/MessageBuilderUtil.sh
@@ -5,10 +5,8 @@ MessageBuilderUtil(){
 	local time=$(BaseUtil timestamp)
 
 	buildMessage(){
-		_message=$(StringUtil capitalize ${2})
-
 		echo "${time} [$(StringUtil toUpperCase ${1})] $(StringUtil
-			replace ${_message} - space)"
+			replace $(StringUtil capitalize ${2}) - space)"
 	}
 
 	$@
