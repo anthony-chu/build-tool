273e8e03da431a002b78af1f186c119052c567e8
Anthony Chu <anthony.chu@liferay.com>
2016-09-01 11:14:56 -0700

    Check for parent directory and not a file

--- lib/package.sh
+++ lib/package.sh
@@ -1,7 +1,7 @@
 package(){
 	local dir=${1}
 
-	local files=($(find * -type f -iname '*.sh' | grep ${dir}))
+	local files=($(find * -type f -iname '*.sh' | grep ${dir}/))
 
 	for file in ${files[@]}; do
 		source ${file}
