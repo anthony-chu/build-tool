a191ac7a04630874a4935d5deb84ebd45526695b
Anthony Chu <anthony.chu@liferay.com>
2016-09-09 14:44:11 -0700

    Override should assume appServer is tomcat

--- App/Server/Version/AppServerVersion.sh
+++ App/Server/Version/AppServerVersion.sh
@@ -3,22 +3,16 @@ include App/Server/Version/Constants/AppServerVersionConstants.sh
 AppServerVersion(){
 	overrideTomcatVersion(){
 		branch=${1}
-		appServer=${2}
 
-		if [[ $(AppServerValidator isTomcat ${appServer}) ]]; then
 			if [[ $(StringValidator isSubstring ${branch} 6.2.x) ]]; then
 				appServerVersion=7.0.62
 			elif [[ $(StringValidator isSubstring ${branch} 6.2.10) ]]; then
 				appServerVersion=7.0.42
 			elif [[ $(StringValidator isSubstring ${branch} 6.1.x) ]]; then
 				appServerVersion=7.0.40
-			else
-				appServerVersion=$(returnAppServerVersion ${appServer})
-			fi
 		else
-			appServerVersion=$(returnAppServerVersion ${appServer})
+			AppServerVersionConstants tomcatVersion
 		fi
-
 	}
 
 	returnAppServerVersion(){
