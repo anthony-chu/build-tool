806437f1a1ff166b136c6612173dcc214042b889
Anthony Chu <anthony.chu@liferay.com>
2016-09-09 14:36:37 -0700

    Move override to AppServerVersion

--- build.sh
+++ build.sh
@@ -178,18 +178,6 @@ _rebuild_db(){
 	cd ${baseDir}
 }
 
-_overrideTomcatVersion(){
-	if [[ $(AppServerValidator isTomcat ${appServer}) ]]; then
-		if [[ $(StringValidator isSubstring ${branch} 6.2.x) ]]; then
-			appServerVersion=7.0.62
-		elif [[ $(StringValidator isSubstring ${branch} 6.2.10) ]]; then
-			appServerVersion=7.0.42
-		elif [[ $(StringValidator isSubstring ${branch} 6.1.x) ]]; then
-			appServerVersion=7.0.40
-		fi
-	fi
-}
-
 build(){
 	local appServer=${appServer}
 
