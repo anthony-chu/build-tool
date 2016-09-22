8049b78857006366ee1ce6cc5ab9f608ff37ac6c
Anthony Chu <anthony.chu@liferay.com>
2016-09-22 10:39:48 -0700

    Use StringUtil

--- file/name/util/FileNameUtil.sh
+++ file/name/util/FileNameUtil.sh
@@ -6,8 +6,7 @@ FileNameUtil(){
 		local path=${1}
 
 		if [[ $(OSValidator isWindows) ]]; then
-			_drive=${path:1:1}
-			drive=${_drive^}
+			drive=$(StringUtil capitalize ${path:1:1})
 			headlessPath=${path/\/[a-z]/}
 
 			echo ${drive}:${headlessPath}
