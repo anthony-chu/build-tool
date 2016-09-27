847a8d1aec50222aa6e584fa0c27ccc0f4117e6e
Anthony Chu <anthony.chu@liferay.com>
2016-09-27 09:56:17 -0700

    Use correct function

--- base/vars/BaseVars.sh
+++ base/vars/BaseVars.sh
@@ -26,11 +26,11 @@ BaseVars(){
 	}
 
 	returnBuildDir(){
-		echo "d:/$(_returnPrivacy $@)/$(_returnBranch $@)-portal"
+		echo "d:/$(_returnPrivacy $@)/$(returnBranch $@)-portal"
 	}
 
 	returnBundleDir(){
-		echo "d:/$(_returnPrivacy $@)/$(_returnBranch $@)-bundles"
+		echo "d:/$(_returnPrivacy $@)/$(returnBranch $@)-bundles"
 	}
 
 	$@
