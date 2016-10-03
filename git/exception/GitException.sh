2a436ccc95b46294f89c0435db778add2e6c1a23
Anthony Chu <anthony.chu@liferay.com>
2016-10-03 08:14:47 -0700

    Apply correct message format

--- git/exception/GitException.sh
+++ git/exception/GitException.sh
@@ -5,14 +5,14 @@ GitException(){
 		action=${1}
 		branch=${2}
 
-		Logger logErrorMsg cannot-${action}-${branch}-because-${branch}-is-the-current-branch.
+		Logger logErrorMsg cannot_${action}_${branch}_because_${branch}_is_the_current_branch
 	}
 
 	existingBranchException(){
 		action=${1}
 		branch=${2}
 
-		Logger logErrorMsg cannot-${action}-${branch}-because-${branch}-already-exists.
+		Logger logErrorMsg cannot_${action}_${branch}_because_${branch}_already_exists
 	}
 
 	$@
