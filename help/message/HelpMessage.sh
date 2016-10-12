8d511c27d3bc3389a27076d6144f73b8218250d9
Anthony Chu <anthony.chu@liferay.com>
2016-10-12 16:55:21 -0700

    Match other message printouts using _ instead of -

--- help/message/HelpMessage.sh
+++ help/message/HelpMessage.sh
@@ -15,7 +15,7 @@ HelpMessage(){
 
 			local helpMessage=$(StringUtil capitalize ${helpList[i]})
 
-			local helpListEntry=$(StringUtil replace ${helpMessage} - space)
+			local helpListEntry=$(StringUtil replace ${helpMessage} _ space)
 
 			echo -e "\t${funcListEntry}................${helpListEntry}"
 		done
@@ -41,19 +41,19 @@ HelpMessage(){
 		local newFuncList=($(ArrayUtil appendArrayEntry ${funcList[@]}))
 
 		local helpList=(
-			displays-all-changes-made-to-the-current-branch
-			displays-the-current-branch
-			deletes-the-branch
-			fetches-a-developer\'s-branch
-			prints-a-formatted-jira-message
-			displays-all-local-branches
-			shows-the-log-for-the-current-branch
-			creates-and-switches-to-a-new-branch
-			provides-options-for-interactive-rebase
-			renames-the-current-branch
-			restores-source-to-designated-commit
-			changes-to-a-different-local-branch
-			provides-direct-shell-access-to-git-directory
+			displays_all_changes_made_to_the_current_branch
+			displays_the_current_branch
+			deletes_the_branch
+			fetches_a_developer\'s_branch
+			prints_a_formatted_jira_message
+			displays_all_local_branches
+			shows_the_log_for_the_current_branch
+			creates_and_switches_to_a_new_branch
+			provides_options_for_interactive_rebase
+			renames_the_current_branch
+			restores_source_to_designated_commit
+			changes_to_a_different_local_branch
+			provides_direct_shell_access_to_git_directory
 		)
 
 		_printHelpMessage ${newFuncList[@]} ${helpList[@]}
@@ -74,14 +74,14 @@ HelpMessage(){
 		local newFuncList=($(ArrayUtil appendArrayEntry ${funcList[@]}))
 
 		local helpList=(
-			builds-bundle-on-specified-app-server
-			rebuilds-database-and-prepares-bundle
-			deploys-the-specified-module-to-bundle
-			pulls-from-upstream-master
-			pushes-current-branch-to-origin
-			rebuilds-app-server-based-on-clean-and-compiled-code
-			runs-a-bundle-on-specified-app-server
-			zips-a-bundle-on-specified-app-server
+			builds_bundle_on_specified_app_server
+			rebuilds_database_and_prepares_bundle
+			deploys_the_specified_module_to_bundle
+			pulls_from_upstream_master
+			pushes_current_branch_to_origin
+			rebuilds_app_server_based_on_clean_and_compiled_code
+			runs_a_bundle_on_specified_app_server
+			zips_a_bundle_on_specified_app_server
 		)
 
 		_printHelpMessage ${newFuncList[@]} ${helpList[@]}
@@ -100,12 +100,12 @@ HelpMessage(){
 		local newFuncList=($(ArrayUtil appendArrayEntry ${funcList[@]}))
 
 		local helpList=(
-			builds-content-targeting-modules
-			removes-content-targeting-modules-from-bundle
-			removes-content-targeting-modules-from-dist-directory
-			returns-the-GIT-ID-of-the-specified-branch
-			generates-a-zip-of-the-content-targeting-jars
-			updates-content-targeting-to-HEAD-on-current-branch
+			builds_content_targeting_modules
+			removes_content_targeting_modules_from_bundle
+			removes_content_targeting_modules_from_dist_directory
+			returns_the_GIT_ID_of_the_specified_branch
+			generates_a_zip_of_the_content_targeting_jars
+			updates_content_targeting_to_HEAD_on_current_branch
 		)
 
 		_printHelpMessage ${newFuncList[@]} ${helpList[@]}
@@ -123,11 +123,11 @@ HelpMessage(){
 		local newOptList=$(ArrayUtil appendArrayEntry ${optList[@]})
 
 		local helpList=(
-			lists-all-dependencies-for-a-given-file
-			lists-all-available-methods
-			prints-this-help-message
-			lists-all-methods-from-a-given-file
-			lists-all-sourceable-files
+			lists_all_dependencies_for_a_given_file
+			lists_all_available_methods
+			prints_this_help_message
+			lists_all_methods_from_a_given_file
+			lists_all_sourceable_files
 		)
 
 		_printHelpMessage ${newOptList} ${helpList[@]}
@@ -144,10 +144,10 @@ HelpMessage(){
 		local newFuncList=($(ArrayUtil appendArrayEntry ${funcList[@]}))
 
 		local helpList=(
-			submits-a-pull-request
-			formats-source-files
-			runs-poshi-validation
-			executes-a-frontend-test
+			submits_a_pull_request
+			formats_source_files
+			runs_poshi_validation
+			executes_a_frontend_test
 		)
 
 		_printHelpMessage ${newFuncList[@]} ${helpList[@]}
