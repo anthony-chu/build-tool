cb12cd9b7426d68af734868934701fdf18bf6871
Anthony Chu <anthony.chu@liferay.com>
2016-10-14 09:42:21 -0700

    Make database interactions their own methods

---
+++ database/Database.sh
@@ -0,0 +1,28 @@
+include base.comparator.BaseComparator
+include database.util.DatabaseUtil
+include string.validator.StringValidator
+
+Database(){
+	create(){
+		if [[ $(StringValidator isNull ${2}) ]]; then
+			cmd="create database ${1};"
+		elif [[ $(BaseComparator isEqual ${2} utf8) ]]; then
+			cmd="create database ${1} utf ${2};"
+		fi
+
+		DatabaseUtil getMysqlCmd ${cmd}
+	}
+
+	drop(){
+		cmd="drop database if exists ${1};"
+
+		DatabaseUtil getMysqlCmd ${cmd}
+	}
+
+	rebuild(){
+		drop ${1}
+		create ${1} ${2}
+	}
+
+	$@
+}
