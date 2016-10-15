7c384f4c2dd523c353cae97bd5b2433479a17795
Anthony Chu <anthony.chu@liferay.com>
2016-10-14 17:04:29 -0700

    Simplify gitpr

--- base/util/BaseUtil.sh
+++ base/util/BaseUtil.sh
@@ -18,8 +18,6 @@ BaseUtil(){
 	}
 
 	gitpr(){
-		alias gitpr="source d:/git-tools/git-pull-request/git-pull-request.sh"
-		source "c:/users/liferay/.bashrc"
 		source "d:/git-tools/git-pull-request/git-pull-request.sh"
 	}
 
