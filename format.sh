6bb940e513852b505ca0c00fcf8368dc48e46f95
Anthony Chu <anthony.chu@liferay.com>
2016-10-28 12:04:52 -0700

    Add formatting checks and validations for shell scripts

---
+++ format.sh
@@ -0,0 +1,44 @@
+source ${projectDir}.init.sh
+
+include array.validator.ArrayValidator
+include file.util.FileUtil
+include finder.Finder
+include logger.Logger
+include string.validator.StringValidator
+
+Format(){
+	verifyNoIncludesInBase(){
+		file=${1}
+
+		if [[ $(StringValidator isSubstring ${file} Base) && ! $(StringValidator
+			isSubstring ${file} Test) ]]; then
+
+			if [[ $(FileUtil getContent ${file}) =~ include ]]; then
+				Logger logErrorMsg illegal_include:_base_scripts_cannot_include_other_scripts:_${file}
+			fi
+		fi
+	}
+
+	verifyNoPackagesInBase(){
+		file=${1}
+
+		if [[ $(StringValidator isSubstring ${file} Base) && ! $(StringValidator
+			isSubstring ${file} Test) ]]; then
+
+			if [[ $(FileUtil getContent ${file}) =~ package ]]; then
+				Logger logErrorMsg illegal_include:_base_scripts_cannot_use_other_script_packages:_${file}
+			fi
+		fi
+	}
+
+	$@
+}
+
+files=($(Finder findBySubstring sh))
+tasks=(verifyNoIncludesInBase verifyNoPackagesInBase)
+
+for file in ${files[@]}; do
+	for task in ${tasks[@]}; do
+		Format ${task} ${file}
+	done
+done
