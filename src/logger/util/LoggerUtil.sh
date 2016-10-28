0c5571c6926386687b948787d440e6fbfd581767
Anthony Chu <anthony.chu@liferay.com>
2016-10-28 12:27:03 -0700

    Log error messages as red.

--- src/logger/util/LoggerUtil.sh
+++ src/logger/util/LoggerUtil.sh
@@ -22,8 +22,14 @@ LoggerUtil(){
 	getLogMsg(){
 		local time=$(BaseUtil timestamp log)
 
-		echo "[${time}] [ $(_formatLogLevel ${1}) ] $(StringUtil
-			replace $(StringUtil capitalize ${2}) _ space)"
+		if [[ $(BaseComparator isEqualIgnoreCase ${1} error) ]]; then
+			message="\033[0;31m$(StringUtil replace $(StringUtil
+				capitalize ${2}) _ space)\033[0m"
+		else
+			message=$(StringUtil replace $(StringUtil capitalize ${2}) _ space)
+		fi
+
+		echo -e "[${time}] [ $(_formatLogLevel ${1}) ] ${message}"
 	}
 
 	$@
