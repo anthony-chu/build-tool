1403a022a9c6991968dec862f841e89b3d65bf9a
Anthony Chu <anthony.chu@liferay.com>
2016-11-09 11:44:23 -0800

    Update formatting rules

--- format.sh
+++ format.sh
@@ -54,18 +54,6 @@ Format(){
 		fi
 	}
 
-	verifyNoPackagesInBase(){
-		file=${1}
-
-		if [[ $(StringValidator isSubstring ${file} Base) && ! $(StringValidator
-			isSubstring ${file} Test) ]]; then
-
-			if [[ $(FileUtil getContent ${file}) =~ package ]]; then
-				Logger logErrorMsg "illegal_package:_${file}"
-			fi
-		fi
-	}
-
 	$@
 }
 
@@ -78,7 +66,6 @@ tasks=(
 	convertSpacesToTabs
 	enforceLoggerMessageQuotes
 	verifyNoIncludesInBase
-	verifyNoPackagesInBase
 )
 
 for file in ${files[@]}; do
