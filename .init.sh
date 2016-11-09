072199272f4c7964513831287337a72d19d70cec
Anthony Chu <anthony.chu@liferay.com>
2016-11-09 11:44:14 -0800

    Remove package library

--- .init.sh
+++ .init.sh
@@ -1,3 +1,2 @@
 source ${projectDir}lib/colorme.sh
 source ${projectDir}lib/include.sh
-source ${projectDir}lib/package.sh
--- lib/package.sh
+++
@@ -1,9 +0,0 @@
-package(){
-	local dir=${1}
-
-	local files=($(find src -type f -iname '*.sh' | grep ${dir//\./\/}))
-
-	for file in ${files[@]}; do
-		source ${file}
-	done
-}
