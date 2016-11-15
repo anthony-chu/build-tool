80b8bc987a576db16de670897a3b5f2cdb3cad4e
Anthony Chu <anthony.chu@liferay.com>
2016-11-14 16:40:32 -0800

    Add validation to keep lines under 80 characters

--- format.sh
+++ format.sh
@@ -1,10 +1,12 @@
 source bash-toolbox/init.sh
 
 include array.validator.ArrayValidator
+include base.comparator.BaseComparator
 include file.util.FileUtil
 include file.io.util.FileIOUtil
 include finder.Finder
 include logger.Logger
+include string.util.StringUtil
 include string.validator.StringValidator
 
 Format(){
@@ -40,6 +42,26 @@ Format(){
 		done < ${file}
 	}
 
+	verifyCharacterLimitPerLine(){
+		file=${1}
+
+		lineNumber=1
+
+		while read line; do
+			length=$(StringUtil length ${line})
+
+			if [[ ${length} -le 9 ]]; then
+				length=$(StringUtil append 0 ${length})
+			fi
+
+			if [[ ${length} > 80 ]]; then
+				Logger logErrorMsg "char_limit_exceeded:_${file}:${lineNumber}"
+			fi
+
+			lineNumber=$((${lineNumber}+1))
+		done < ${file}
+	}
+
 	verifyNoIncludesInBase(){
 		file=${1}
 
@@ -63,6 +85,7 @@ tasks=(
 	applyUnixLineEndings
 	convertSpacesToTabs
 	enforceLoggerMessageQuotes
+	verifyCharacterLimitPerLine
 	verifyNoIncludesInBase
 )
 
