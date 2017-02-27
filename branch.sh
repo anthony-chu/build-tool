bd100b9259dac56b013fc351588d249b10058378
Anthony Chu <anthony.chu@liferay.com>
2017-02-27 14:20:26 -0800

    Extract rebase impl into its own subshell

--- branch.sh
+++ branch.sh
@@ -154,65 +154,6 @@ new(){
 }
 
 rebase(){
-	abort(){
-		Logger logProgressMsg "terminating_previous_rebase_process"
-
-		cd ${buildDir}
-
-		git rebase --abort
-
-		Logger logCompletedMsg
-	}
-
-	amend(){
-		Logger logProgressMsg "amending_the_previous_commit"
-
-		cd ${buildDir}
-
-		git commit --amend
-
-		Logger logCompletedMsg
-	}
-
-	cont(){
-		Logger logProgressMsg "continuing_the_current_rebase_process"
-
-		cd ${buildDir}
-
-		git rebase --continue
-
-		Logger logCompletedMsg
-	}
-
-	default(){
-		cd ${buildDir}
-
-		curBranch=$(GitUtil getCurBranch)
-
-		Logger logProgressMsg "rebasing_${curBranch}_against_${branch}_HEAD"
-
-		git pull --rebase upstream ${branch}
-
-		Logger logCompletedMsg
-	}
-
-	start(){
-		local value=$(StringUtil returnOption ${1})
-
-		local _message=(
-			rebasing_the_last_${value}_
-			$(LanguageUtil togglePlurality ${value} commit commits)
-		)
-
-		Logger logProgressMsg "$(StringUtil build _message)"
-
-		cd ${buildDir}
-
-		git rebase -i head~${value}
-
-		Logger logCompletedMsg
-	}
-
 	if [[ $(StringValidator isNull ${1}) ]]; then
 		Logger logErrorMsg "please_provide_a_valid_rebase_option"
 		exit
