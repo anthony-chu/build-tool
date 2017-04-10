c43fd93202bf8c73604a0cf7b7f6e586f59b0d16
Anthony Chu <anthony.chu@liferay.com>
2017-04-10 11:01:45 -0700

    File creation already included when PropsWriter calls FileNameUtil.getPath()

--- test.sh
+++ test.sh
@@ -141,9 +141,6 @@ test(){
 	if [[ $(StringValidator isNull ${1}) ]]; then
 		Logger logErrorMsg "missing_test_name"
 	else
-		local propsFile=$(FileUtil
-			makeFile ${testDir}/test.${HOSTNAME}.properties)
-
 		PropsWriter setTestProps ${branch} timeout.explicit.wait 60
 
 		test=${1}
