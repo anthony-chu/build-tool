8a212ee3d32c31a763f72497e4f4bb5b56e0b13e
Anthony Chu <anthony.chu@liferay.com>
2017-12-13 10:03:12 -0800

    Skip variable scope validation for ignored files

--- format.sh
+++ format.sh
@@ -6,12 +6,16 @@ include formatter.Formatter
 
 include logger.Logger
 
+include props.reader.util.PropsReaderUtil
+
 include string.util.StringUtil
 include string.validator.StringValidator
 
 _main(){
 	Logger logProgressMsg "validating_formatting_rules"
 
+	local skipVariableValidation=$(PropsReaderUtil
+		readProps $(pwd)/formatter.properties ignore_local_variables)
 	local files=($(find * -type f -iname "*.sh"))
 
 	local tasks=(
@@ -25,7 +29,13 @@ _main(){
 
 	for file in ${files[@]}; do
 		for task in ${tasks[@]}; do
-			Formatter ${task} ${file}
+			if [[ ${task} == enforceLocalVariables &&
+				! ${skipVariableValidation} =~ ${file} ]]; then
+
+				Formatter ${task} ${file}
+			else
+				Formatter ${task} ${file}
+			fi
 		done
 	done
 
