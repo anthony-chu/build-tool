b7f1f744a1d703b93f0ee303c0a87f68ee6d523e
Anthony Chu <anthony.chu@liferay.com>
2018-09-20 11:22:43 -0700

    Refactor: nightly bundle no longer uses includes tomcat version

--- nightly.sh
+++ nightly.sh
@@ -1,17 +1,15 @@
 source bash-toolbox/init.sh
 
-include app.server.validator.AppServerValidator
 include app.server.version.AppServerVersion
 
 include base.comparator.BaseComparator
 include base.vars.BaseVars
 
-include bundle.util.BundleUtil
-
 include command.validator.CommandValidator
 
 include database.Database
 
+include file.name.util.FileNameUtil
 include file.util.FileUtil
 include file.writer.FileWriter
 
@@ -21,116 +19,122 @@ include logger.Logger
 
 include props.writer.util.PropsWriterUtil
 
-include string.util.StringUtil
 include string.validator.StringValidator
 
-@description rebuids_database_and_prepares_bundle_for_runtime
-clean(){
-	for dir in {data,logs,osgi/state}; do
-		rm -rf ${nightlyDir}/${dir}
-	done
-
-	local appServerVersion=$(AppServerVersion
-		getAppServerVersion ${appServer} ${branch})
-
-	local appServerDir=${nightlyDir}/${appServer}-${appServerVersion}
-
-	for dir in {temp,work}; do
-		rm -rf ${appServerDir}/${dir}
-	done
-
-	Database rebuild ${baseDatabase}nightly utf8
-}
-
-@description downloads_a_nightly_Tomcat_bundle_on_the_indicated_branch
+@description downloads_nightly_tomcat_bundle_on_indicated_branch
 get(){
 	cd ${buildDir}
 
+	${_log} info "downloading_nightly_bundle..."
+
 	ant snapshot-bundle
 
-	${_log} info "completed"
+	${_log} info "completed."
 
-	BundleUtil configure ${branch} ${appServer}
+	${_log} info "renaming_tomcat_directory..."
+
+	local bundleAppServerDir=${bundleDir}/${APP_SERVER}-${appServerVersion}
+
+	FileUtil deleteIfExists ${bundleAppServerDir}
+
+	mv ${bundleDir}/${APP_SERVER} ${bundleAppServerDir}
+
+	${_log} info "completed."
+
+	${_log} info "applying_custom_memory_settings..."
+
+	local file=${bundleAppServerDir}/bin/setenv.sh
+
+	${replace} ${file} 'Xmx[0-9]\+m' 'Xms1024m\\ -Xmx2048m'
+
+	${_log} info "completed."
 
 	if [[ -e ${nightlyDir} ]]; then
-		${_log} info "cleaning_out_nightly_directory"
+		${_log} info "cleaning_out_nightly_directory..."
 
 		rm -rf ${nightlyDir}/*
 	else
-		${_log} info "constructing_nightly_directory"
+		${_log} info "constructing_nightly_directory..."
 
-		local _nightlyDir=$(FileUtil construct ${nightlyDir})
+		mkdir -p ${nightlyDir}
 	fi
 
-	${_log} info "copying_${branch}_nightly_bundle_to_nightly_directory..."
+	${_log} info "completed."
 
-	local fileDirs=(
-		.githash
+	${_log} info "copying_from_bundle_directory_to_nightly_directory..."
+
+	local filePaths=(
+		data
 		deploy
 		license
+		logs
 		osgi
-		${appServer}-$(AppServerVersion
-			getAppServerVersion ${appServer} ${branch})
+		${APP_SERVER}-${appServerVersion}
 		tools
 		work
+		.githash
+		.liferay-home
 		portal-ext.properties
 	)
 
-	for fileDir in ${fileDirs[@]}; do
-		local _fileDir=${bundleDir}/${fileDir}
+	for _filePath in ${filePaths[@]}; do
+		local filePath=${bundleDir}/${_filePath}
 
-		if [[ -e ${_fileDir} || -d ${_fileDir} ]]; then
-			cp -rf ${bundleDir}/${fileDir} -d ${nightlyDir}
+		if [[ -e ${filePath} || -d ${filePath} ]]; then
+			cp -rf ${filePath} -d ${nightlyDir}
 		fi
 	done
 
+	${_log} info "completed."
+
 	${_log} info "updating_portal_properties"
 
-	local props=${nightlyDir}/portal-ext.properties
+	local propsFile=${nightlyDir}/portal-ext.properties
 
-	FileWriter replace ${props} ${baseDatabase} ${baseDatabase}nightly
-	PropsWriterUtil setProps ${props} liferay.home $(
-			FileNameUtil getPath 1 ${nightlyDir})
+	${replace} ${propsFile} ${baseDatabase} ${nightlyDatabase}
 
-	${_log} info "completed"
+	local _nightlyDir=$(FileNameUtil getPath 1 ${nightlyDir})
+
+	PropsWriterUtil setProps ${propsFile} liferay.home ${_nightlyDir}
+
+	${_log} info "completed."
+
+	Database rebuild ${nightlyDatabase} utf8
 }
 
-@description starts_up_a_nightly_bundle
+@description starts_up_nightly_bundle
 run(){
-	local _appServer=$(StringUtil toUpperCase ${appServer})
-	local appServerVersion=$(AppServerVersion
-		getAppServerVersion ${appServer} ${branch})
+	${_log} info "starting_up_${APP_SERVER}_nightly_bundle"
 
-	${_log} info "starting_up_a_${_appServer}_nightly_bundle"
-
-	if [[ $(AppServerValidator isTomcat ${appServer}) ]]; then
-		${nightlyDir}/${appServer}-${appServerVersion}/bin/catalina.sh run
-	fi
+	${nightlyDir}/${APP_SERVER}/${appServerVersion}/catalina.sh run
 }
 
 main(){
-	local _log="Logger log"
+	local APP_SERVER=tomcat
+	local branch=$(BaseVars getBranch $@)
 
-	@param the_app_server_\(optional\)
-	local appServer=tomcat
+	local appServerVersion=$(
+		AppServerVersion getAppServerVersion ${APP_SERVER} ${branch}
+	)
 
+	local baseDatabase=lportal${branch//[-.]/}
 	local baseDir=$(pwd)
 
-	@param the_branch_name_\(optional\)
-	local branch=$(BaseVars getBranch $@)
 	local buildDir=$(BaseVars getBuildDir ${branch})
 	local bundleDir=$(BaseVars getBundleDir ${branch})
 
-	local baseDatabase=lportal${branch//[-.]/}
-
+	local nightlyDatabase=${baseDatabase}nightly
 	local nightlyDir=${HOME}/Desktop/nightly/${branch}/bundles
+
+	local _log="Logger log"
+	local replace="FileWriter replace"
 
 	if [[ $(StringValidator isNull ${1}) ]]; then
 		HelpMessage printHelpMessage
 	else
 		until [[ $(StringValidator isNull ${1}) ]]; do
 			if [[ $(BaseComparator isEqual ${1} ${appServer}) || $(
-				BaseComparator isEqual ${1} ${branch}) ]]; then
+					BaseComparator isEqual ${1} ${branch}) ]]; then
 
 				shift
 			else
