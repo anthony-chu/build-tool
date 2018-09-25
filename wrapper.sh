49b0fd0d7ade46caa4503d06a591bc1e1a94a224
Anthony Chu <anthony.chu@liferay.com>
2018-09-25 16:42:57 -0700

    Use directly

--- wrapper.sh
+++ wrapper.sh
@@ -5,15 +5,13 @@ include logger.Logger
 include string.util.StringUtil
 
 main(){
-	local _log="Logger log"
-
 	local _className=${1}
 	local _cmd=${2}
 	shift 2
 	local args=$@
 
 	if [[ ! ${_className} ]]; then
-		${_log} error "please_provide_a_classname_and_command."
+		Logger log error "please_provide_a_classname_and_command."
 		exit
 	fi
 
