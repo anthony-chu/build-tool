6a50e12c0486026c800683209220d4315591ab95
Anthony Chu <anthony.chu@liferay.com>
2018-10-17 10:59:28 -0700

    Clean up unused functions

--- branch.sh
+++ branch.sh
@@ -2,7 +2,6 @@ source bash-toolbox/init.sh
 
 include app.server.validator.AppServerValidator
 
-include base.comparator.BaseComparator
 include base.vars.BaseVars
 
 include command.validator.CommandValidator
@@ -11,69 +10,7 @@ include help.message.HelpMessage
 
 include jira.comment.util.JiraCommentUtil
 
-include logger.Logger
-
-include source.util.SourceUtil
-
-include string.util.StringUtil
 include string.validator.StringValidator
-
-package git
-
-@description displays_all_changes_made_to_current_branch
-changes(){
-	cd ${buildDir}
-
-	git status
-}
-
-@description displays_the_current_branch
-current(){
-	cd ${buildDir}
-
-	${_log} info "current_${branch}_branch:_$(GitUtil getCurBranch)"
-}
-
-@description deletes_the_indicated_branch
-delete(){
-	cd ${buildDir}
-
-	local curBranch=$(GitUtil getCurBranch)
-
-	if [[ $(BaseComparator isEqual ${1} ${curBranch}) ]]; then
-		GitException curBranchException delete ${1}
-	else
-		if ! $(git branch -q -D ${1}); then
-			GitException branchDoesNotExistException delete ${1}
-
-			return
-		fi
-
-		${_log} info "deleted_local_branch:_${1}"
-	fi
-}
-
-@description fetches_a_branch_from_a_pull_request
-fetch(){
-	cd ${buildDir}
-
-	local _id=${1}
-
-	${_log} info "fetching_pr#${_id}_into_${branch}-ac-${_id}..."
-
-	if [[ "$(GitUtil listBranches)" =~ ${branch}-ac-${_id} ]]; then
-		local message=(
-			cannot_fetch_pr#${_id}_because
-			branch_${branch}-ac-${_id}_exists
-		)
-
-		${_log} error"$(StringUtil join message _)"
-	else
-		git fetch origin pull/${_id}/head:${branch}-ac-${_id}
-
-		${_log} info "completed"
-	fi
-}
 
 @description prints_a_formatted_Jira_comment
 jira(){
@@ -95,105 +32,7 @@ jira(){
 	JiraCommentUtil ${cmd} ${args}
 }
 
-@description displays_all_local_branches
-list(){
-	cd ${buildDir}
-
-	GitUtil listBranches
-}
-
-@description displays_the_log_for_the_current_branch
-log(){
-	cd ${buildDir}
-
-	if [[ $(StringValidator isNull ${1}) ]]; then
-		git log -1 --oneline
-	elif [[ $(StringValidator isOption ${1}) ]]; then
-		git log ${1} --oneline
-	elif [[ ! $(StringValidator isOption ${1}) ]]; then
-		${_log} error"please_provide_a_valid_log_option"
-	fi
-}
-
-@description creates_and_switches_to_the_indicated_branch
-new(){
-	cd ${buildDir}
-
-	local branches=($(GitUtil listBranches))
-
-	if [[ $(ArrayValidator hasEntry branches ${1}) ]]; then
-		GitException existingBranchException create ${1}
-	else
-		git checkout -q -b ${1}
-	fi
-
-	current
-}
-
-@description provides_options_for_and_executes_an_interative_rebase
-rebase(){
-	SourceUtil clearGradleCache ${branch}
-
-	case $(StringUtil returnOption ${1}) in
-		[0-9]*) local cmd="start ${1}";;
-		a) local cmd=amend;;
-		c) local cmd=cont;;
-		d) local cmd=default;;
-		q) local cmd=abort;;
-		*) ${_log} error"please_provide_a_valid_rebase_option" && exit ;;
-	esac
-
-	GitRebaseUtil ${cmd} ${branch}
-}
-
-@description renames_the_current_branch
-rename(){
-	cd ${buildDir}
-
-	${_log} info "renaming_branch_from_$(GitUtil getCurBranch)_to_${1}..."
-
-	git branch -q -m ${1}
-
-	${_log} info "completed"
-}
-
-@description restores_source_code_the_designated_commit
-reset(){
-	cd ${buildDir}
-
-	if [[ $(StringValidator isAlphaNum ${1}) ]]; then
-		local commit=${1}
-	fi
-
-	git reset --hard ${commit}
-}
-
-@description changes_to_a_different_local_branch
-switch(){
-	SourceUtil clearGradleCache ${branch}
-
-	cd ${buildDir}
-
-	if [[ $(StringValidator isNull ${1}) ]]; then
-		local b=master
-	else
-		local b=${1}
-	fi
-
-	local allBranches=($(GitUtil listBranches))
-
-	if [[ ! $(StringValidator isSubstring allBranches b) ]]; then
-		GitException noSuchBranchException switch_to ${b}
-	else
-		git checkout -q ${b}
-	fi
-
-	current
-}
-
 main(){
-	local _log="Logger log"
-
 	@param the_app_server_\(optional\)
 	local appServer=$(AppServerValidator returnAppServer ${@})
 
@@ -205,8 +44,6 @@ main(){
 	if [[ $(StringValidator isNull ${1}) ]]; then
 		HelpMessage printHelpMessage
 	else
-		GitUtil clearIndexLock ${branch}
-
 		CommandValidator validateCommand ${0} ${1}
 
 		${@}
