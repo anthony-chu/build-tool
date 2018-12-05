ec6dd8b3f431603e9aa53a4a32a5074131fe5182
Anthony Chu <anthony.chu@liferay.com>
2018-12-05 11:02:59 -0800

    Fix class

--- nightly.sh
+++ nightly.sh
@@ -42,7 +42,7 @@ get(){
 	fi
 
 	PropsWriter setAppServerProps ${branch} app.server.type ${APP_SERVER}
-	Propswriter setAppServerProps ${branch} app.server.version $(
+	PropsWriter setAppServerProps ${branch} app.server.version $(
 		AppServerVersion getAppServerVersion ${APP_SERVER} ${branch})
 
 	ant snapshot-bundle
